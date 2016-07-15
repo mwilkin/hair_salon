70e1b792725485f76eeeacac8ffd6595c56e26d3
Matt Wilkin <mwilkin22@gmail.com>
2016-07-15 09:44:41 -0700

    add code and spec for Client#initialize with a name.

--- lib/client.rb
+++ lib/client.rb
@@ -3,6 +3,7 @@ class Client
   attr_reader(:id, :name, :phone_number)
 
   define_method(:initialize) do | attributes |
+    @name = attributes[:name]
 
 
 
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -7,7 +7,13 @@ describe('Client') do
       expect(client.class).to eq(Client)
     end
 
+    it('instantiates a client with a name') do
+      client = Client.new({:name => 'Skip'})
+      expect(client.name).to eq('Skip')
+    end
+
   end
+
 
 
 end
