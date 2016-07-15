a7cbcb96af57134e74ebe69c20991e0287d4973f
Matt Wilkin <mwilkin22@gmail.com>
2016-07-15 16:56:45 -0700

    begin to establish routes/paths between client views.

--- app.rb
+++ app.rb
@@ -1,10 +1,23 @@
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
+require('./lib/client')
+require('./lib/stylist')
 require('pg')
+require('pry')
 
 get ('/') do
-
-
   erb(:index)
+end
+
+get ('/clients') do
+  erb(:clients)
+end
+
+get ('/client_view') do
+  erb(:)
+end
+
+post ('/client_update') do
+  erb(:)
 end
--- spec/client_integration_spec.rb
+++ spec/client_integration_spec.rb
@@ -9,3 +9,11 @@ describe('root path', {:type => :feature}) do
     expect(page).to have_content('Chop Hair Salon')
   end
 end
+
+describe('path to view clients', {:type => :feature}) do
+  it('displays list of clients') do
+    visit('/')
+    click_link('Clients')
+    expect(page).to have_content('Client List')
+  end
+end
