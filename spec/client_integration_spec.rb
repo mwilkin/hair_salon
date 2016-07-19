107a3221a4caf032510699da9bbe8e4503856c27
Matt Wilkin <mwilkin22@gmail.com>
2016-07-19 04:47:30 -0700

    add integration_spec and route to show client list page.

--- spec/client_integration_spec.rb
+++ spec/client_integration_spec.rb
@@ -9,3 +9,11 @@ describe('root path', {:type => :feature}) do
     expect(page).to have_content('Chop Shop Salon')
   end
 end
+
+describe('path to view client list', {:type => :feature}) do
+  it('displays list of clients') do
+    visit('/')
+    click_link('Clients')
+    expect(page).to have_content('Client List')
+  end
+end
