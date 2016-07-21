817103e527f8c0ed6a56a79f57882ddee1c82c50
Matt Wilkin <mwilkin22@gmail.com>
2016-07-21 05:01:13 -0700

    add integration tests.

--- spec/client_integration_spec.rb
+++ spec/client_integration_spec.rb
@@ -19,17 +19,46 @@ describe('path to view client list', {:type => :feature}) do
   end
 end
 
-describe('add a client', {:type => :feature}) do
-  it('allows user to add a new client') do
-    stylist = Stylist.new({:id => nil, :name => "Le Chopper", :phone_number => "101-101-1001"})
-    stylist.save()
-    visit('/')
-    click_link('Clients')
-    fill_in('name', :with => 'Tom Jones')
-    fill_in('phone_number', :with => '541-123-3214')
-    select('Le Chopper', :from => 'stylist_id')
-    click_button('Add Client')
-    expect(page).to have_content('Tom Jones')
+# describe('add a client', {:type => :feature}) do
+#   it('allows user to add a new client') do
+#     stylist = Stylist.new({:id => nil, :name => "Le Chopper", :phone_number => "101-101-1001"})
+#     stylist.save()
+#     visit('/')
+#     click_link('Clients')
+#     fill_in('name', :with => 'Tom Jones')
+#     fill_in('phone_number', :with => '541-123-3214')
+#     select('Le Chopper', :from => 'stylist_id')
+#     click_button('Add Client')
+#     expect(page).to have_content('Tom Jones')
+#   end
+# end
 
-  end
-end
+# describe('delete a client', {:type => :feature}) do
+#   it('allows user to delete a client') do
+#     stylist = Stylist.new({:id => nil, :name => "Le Chopper", :phone_number => "101-101-1001"})
+#     stylist.save()
+#     visit('/')
+#     click_link('Clients')
+#     click_link('Le Chopper')
+#     click_button('Delete Client')
+#     expect(page).to have_content('There are no clients currently.')
+#   end
+# end
+
+# describe('path to update a client', {:type => :feature}) do
+#   it('will allow a user to update client information') do
+#     stylist = Stylist.new({:id => nil, :name => "Timmy", :phone_number => "511-541-1515"})
+#     stylist.save()
+    # stylist1 = Stylist.new({:id => nil, :name => "Chipper", :phone_number => "211-555-1005"})
+#     stylist1.save()
+#     client = Client.new({:id => nil, :name => "john", :phone_number => "100-777-9988", :stylist_id => nil})
+#     client.save()
+#     visit('/clients')
+#     click_link('john')
+#     fill_in('new_client_name', :with => "John Black")
+#     fill_in('new_client_phone_number', :with => "444-555-7878")
+#     select('Chipper', :from => 'new_stylist_id')
+#     click_button('Update')
+#     expect(page).to have_content('John Black')
+#   end
+# end
--- spec/stylist_integration_spec.rb
+++ spec/stylist_integration_spec.rb
@@ -20,12 +20,37 @@ describe('path to add a new stylist', {:type => :feature}) do
   end
 end
 
-# describe('add a new stylist', {:type => :feature}) do
-#   it 'allows user to add a new stylist' do
+describe('add a new stylist', {:type => :feature}) do
+  it 'allows user to add a new stylist' do
+    visit('/')
+    click_link('Stylists')
+    fill_in('name', :with => "Le Clipper")
+    fill_in('phone_number', :with => "100-100-1220")
+    click_button('Add Stylist')
+    expect(page).to have_content("Add a Stylist")
+  end
+end
+
+# describe('delete a stylist', {:type => :feature}) do
+#   it('allows the user to delete a stylist') do
+#     stylist = Stylist.new({:id => nil, :name => "Guy", :phone_number => "511-541-1515"})
+#     stylist.save()
+#     visit('/')
+#     click_link('Stylists')
+#     click_link('Guy')
+#     expect(page).to have_content("New Name")
+#   end
+# end
+
+# describe('update a stylist', {:type => :feature}) do
+#   it('allows a user to update a stylists information') do
+#     stylist = Stylist.new({:id => nil, :name => "Timmy", :phone_number => "511-541-1515"})
+#     stylist.save()
 #     visit('/stylists')
-#     fill_in('name', :with => "Santa Claus")
-#     fill_in('phone_number', :with => "100-100-1220")
-#     click_button('Add Stylist')
-#     expect(page).to have_content("Santa Claus")
+#     click_link('Timmy')
+#     fill_in('new_name', :with => "Timmy Jones")
+#     fill_in('new_phone_number', :with => "120-210-1234")
+#     click_button('Update Stylist')
+#     expect(page).to have_content("Timmy Jones")
 #   end
 # end
